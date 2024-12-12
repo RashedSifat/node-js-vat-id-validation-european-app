84a1c1b54f794a0336740dbc6de46b3d260cf68a
Rasheduzzaman Sifat <me.sifat@live.com>
2024-12-12 22:09:36 +0800

    again again

--- netlify/functions/validate.js
+++ netlify/functions/validate.js
@@ -1,5 +1,4 @@
-const soap = require('soap');
-const endpoint = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
+const fetch = require('node-fetch'); // Use axios or fetch as preferred
 
 exports.handler = async (event) => {
     if (event.httpMethod !== 'POST') {
@@ -10,7 +9,7 @@ exports.handler = async (event) => {
     }
 
     try {
-        // Parse the request body
+        // Parse the incoming request body
         const { country, vat } = JSON.parse(event.body || '{}');
 
         if (!country || !vat) {
@@ -22,43 +21,28 @@ exports.handler = async (event) => {
 
         console.log('Input Data:', { country, vat });
 
-        // Helper to create SOAP client
-        const createClient = (endpoint) => {
-            return new Promise((resolve, reject) => {
-                soap.createClient(endpoint, (err, client) => {
-                    if (err) reject(err);
-                    else resolve(client);
-                });
-            });
-        };
+        // REST API URL with placeholders replaced
+        const apiUrl = `https://ec.europa.eu/taxation_customs/vies/rest-api/ms/${country}/vat/${vat}`;
 
-        const client = await createClient(endpoint);
-        const params = { countryCode: country, vatNumber: vat };
+        // Make a request to the REST API
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`VAT validation failed with status ${response.status}`);
+        }
 
-        console.log('SOAP Params:', params);
+        const result = await response.json();
 
-        // Helper to validate VAT
-        const validateVat = (client, params) => {
-            return new Promise((resolve, reject) => {
-                client.checkVat(params, (err, result) => {
-                    if (err) reject(err);
-                    else resolve(result);
-                });
-            });
-        };
+        console.log('API Response:', result);
 
-        const result = await validateVat(client, params);
-
-        console.log('SOAP Response:', result);
-
+        // Format the response for the client
         return {
             statusCode: 200,
             body: JSON.stringify({
                 valid: result.valid,
-                companyName: result.name || 'N/A',
-                companyAddress: result.address || 'N/A',
-                countryCode: result.countryCode,
-                vatNumber: result.vatNumber,
+                companyName: result.traderName || 'N/A',
+                companyAddress: result.traderAddress || 'N/A',
+                countryCode: country,
+                vatNumber: vat,
             }),
         };
     } catch (error) {
@@ -69,3 +53,77 @@ exports.handler = async (event) => {
         };
     }
 };
+
+
+
+// const soap = require('soap');
+// const endpoint = 'http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl';
+
+// exports.handler = async (event) => {
+//     if (event.httpMethod !== 'POST') {
+//         return {
+//             statusCode: 405,
+//             body: 'Method Not Allowed',
+//         };
+//     }
+
+//     try {
+//         // Parse the request body
+//         const { country, vat } = JSON.parse(event.body || '{}');
+
+//         if (!country || !vat) {
+//             return {
+//                 statusCode: 400,
+//                 body: JSON.stringify({ error: 'Country and VAT number are required' }),
+//             };
+//         }
+
+//         console.log('Input Data:', { country, vat });
+
+//         // Helper to create SOAP client
+//         const createClient = (endpoint) => {
+//             return new Promise((resolve, reject) => {
+//                 soap.createClient(endpoint, (err, client) => {
+//                     if (err) reject(err);
+//                     else resolve(client);
+//                 });
+//             });
+//         };
+
+//         const client = await createClient(endpoint);
+//         const params = { countryCode: country, vatNumber: vat };
+
+//         console.log('SOAP Params:', params);
+
+//         // Helper to validate VAT
+//         const validateVat = (client, params) => {
+//             return new Promise((resolve, reject) => {
+//                 client.checkVat(params, (err, result) => {
+//                     if (err) reject(err);
+//                     else resolve(result);
+//                 });
+//             });
+//         };
+
+//         const result = await validateVat(client, params);
+
+//         console.log('SOAP Response:', result);
+
+//         return {
+//             statusCode: 200,
+//             body: JSON.stringify({
+//                 valid: result.valid,
+//                 companyName: result.name || 'N/A',
+//                 companyAddress: result.address || 'N/A',
+//                 countryCode: result.countryCode,
+//                 vatNumber: result.vatNumber,
+//             }),
+//         };
+//     } catch (error) {
+//         console.error('Error:', error.message);
+//         return {
+//             statusCode: 500,
+//             body: JSON.stringify({ error: `Internal Server Error: ${error.message}` }),
+//         };
+//     }
+// };
